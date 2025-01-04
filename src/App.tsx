cb61987af0d4b1193e1c9a6a58b28a81f13894a5
JPChoyon <kotjpckhan@gmail.com>
2025-01-04 21:41:16 +0600

    redux and shadcn ui install done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button>custom button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
