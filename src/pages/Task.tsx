a9546941fb221a0cfaea93efc73673d56fe38682
JPChoyon <kotjpckhan@gmail.com>
2025-01-09 23:26:50 +0600

    task filter added

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,14 +1,47 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import TaskCard from "@/module/Task/TaskCard";
 import { TaskModal } from "@/module/Task/TaskModal";
-import { selectTask } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hook";
+import { selectTask, updateFilter } from "@/redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 const Task = () => {
   const tasks = useAppSelector(selectTask);
+  const dispatch = useAppDispatch();
 
   return (
     <div>
-      <TaskModal></TaskModal>
+      <div className="flex gap-5 justify-end py-4">
+        <Tabs defaultValue="All">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("All"))}
+              value="All"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("Low"))}
+              value="Low"
+            >
+              Low
+            </TabsTrigger>
+
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("Medium"))}
+              value="Medium"
+            >
+              Medium
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("High"))}
+              value="High"
+            >
+              High
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <TaskModal></TaskModal>
+      </div>
 
       {tasks.map((task) => (
         <TaskCard key={task.id} task={task}></TaskCard>
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -5,9 +5,20 @@ import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit";
 
 interface initialState {
   task: ITask[];
+  filter: "All" | "High" | "Low" | "Medium";
 }
 const initialState: initialState = {
-  task: [],
+  task: [
+    {
+      id: "e45g44j1MJmnofOla7vmc",
+      isCompleted: false,
+      title: "Consectetur ratione",
+      description: "Ratione rem voluptat",
+      dueDate: "2025-01-29T18:00:00.000Z",
+      priority: "High",
+    },
+  ],
+  filter: "All",
 };
 type draftTask = Pick<ITask, "title" | "description" | "priority" | "dueDate">;
 const createTask = (taskData: draftTask): ITask => {
@@ -31,10 +42,29 @@ const taskSlice = createSlice({
     deleteTask: (state, action: PayloadAction<string>) => {
       state.task = state.task.filter((task) => task.id !== action.payload);
     },
+    updateFilter: (
+      state,
+      action: PayloadAction<"All" | "High" | "Low" | "Medium">
+    ) => {
+      state.filter = action.payload;
+    },
   },
 });
 export const selectTask = (state: RootState) => {
-  return state.tasks.task;
+  const filter = state.tasks.filter;
+  if (filter === "Low") {
+    return state.tasks.task.filter((task) => task.priority === "Low");
+  } else if (filter === "High") {
+    return state.tasks.task.filter((task) => task.priority === "High");
+  } else if (filter === "Medium") {
+    return state.tasks.task.filter((task) => task.priority === "Medium");
+  } else {
+    return state.tasks.task;
+  }
 };
-export const { addTask, completeTask, deleteTask } = taskSlice.actions;
+export const selectTaskFilter = (state: RootState) => {
+  return state.tasks.filter;
+};
+export const { addTask, completeTask, deleteTask, updateFilter } =
+  taskSlice.actions;
 export default taskSlice.reducer;
